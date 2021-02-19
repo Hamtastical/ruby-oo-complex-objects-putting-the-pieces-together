159d70193eb4604d2db2a95a53f5b1ad178274de
meili <meiliham1@gmail.com>
2021-02-18 21:29:21 -0500

    commit tree 3a3146adb62f1e05898a7a28d71eedb2ff13c06d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,41 @@
+class Book #define a book class
 
+    def initialize (title) #initialize the title of the book (.new goes through the initialize)
+        @title = title #creates an instance variable for the property to be used in the methods within the initialization
+    end
+
+    def title #define a method to take in a title of the book
+        @title #returns the instance variable property to use it in the instance (aka when making a .new or .title)
+    end
+
+    def author=(author) #Setter, used to read the define method of the author to provide an author name
+        @author  = author
+    end
+
+    def author #getter, used to grab and put out the above method. Usually in pair with setter
+        @author
+    end
+
+    def page_count=(num)
+        @page_count = num
+    end
+
+    def page_count
+        @page_count
+    end
+
+    def genre=(genre)
+        @genre = genre
+    end
+
+    def genre
+        @genre
+    
+    end
+
+    def turn_page #behavior method for this class
+        puts "Flipping the page...wow, you read fast!" #puts out string
+    end
+
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,16 @@
 # Make your shoe class here!
+
+class Shoe 
+    attr_accessor :color, :size, :material, :condition #provides, the getter and setter for these since they are properties
+    attr_reader :brand #assigns the getter since it is just reading the method being intialized
+    def initialize(brand)
+        @brand = brand
+    end
+
+    def cobble #behavior method for cobble
+        @condition = "new" #grabs condition insance variable (which is a property) and changes it to new when it is called
+        puts "Your shoe is as good as new!" #puts out string
+    end
+
+
+end
